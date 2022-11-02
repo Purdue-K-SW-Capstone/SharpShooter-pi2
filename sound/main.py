64ea71dac7d0e362d4c4615f8029fa47138f4aff
8471919 <7471919@naver.com>
2022-11-02 09:18:00 -0400

    chore: add annotation to sound/main.py

--- sound/main.py
+++ sound/main.py
@@ -1,6 +1,11 @@
 import pyaudio
 import wave
 
+# CHUNK는 음성 데이터를 불러올 때, 한 번에 몇 개의 정수를
+# 한 번 불러올 때다 1024개의 정수를 불러온다
+# 
+# RATE는 음성 데이터의 샘플링 레이트이다.
+# 높을수록 음질이 좋아진다.
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -10,6 +15,12 @@ WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
 
+# 음성 데이터 스트림을 여는 코드
+# format : 비트 깊이 설정
+# rate : 샘플링 레이트
+# input : 입력 스트림인지 아닌지 설정하는 매개 변수
+# frame_per_buffer : chunk크기
+# input_device_index : 원하는 입력 장치의 번호. 입력 하지 않으면 자동으로 세팅해준다.
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -21,8 +32,16 @@ print("Start to record the audio.")
 frames = []
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    # 음성 데이터를 문자열로 반환
+    # data is bytes type
+    # frames is list type
     data = stream.read(CHUNK)
+    print(type(data))
+    # print("data : " +str(data))
+    
     frames.append(data)
+    print(type(frames))
+    # print("frames : " +str(frames))
 
 print("Recording is finished.")
 
